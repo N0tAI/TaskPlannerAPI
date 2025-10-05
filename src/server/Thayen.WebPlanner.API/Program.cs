f9505cd005f424ee7f9948218d2f631eb7fcd203
Not_AI <44149574+N0tAI@users.noreply.github.com>
2025-10-04 23:14:14 -0400

    Revising to DB First approach

--- src/server/Thayen.WebPlanner.API/Program.cs
+++ src/server/Thayen.WebPlanner.API/Program.cs
@@ -8,11 +8,8 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddDataServices(builder.Configuration.GetConnectionString("PostgresConnection")!);
-
+        builder.Services.AddDataServices(builder.Configuration.GetConnectionString("Database")!);
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
---
+++ src/server/Thayen.WebPlanner.Data/Scripts/setup_tables.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS planner.tasks;
+
+CREATE TABLE planner.tasks (
+    task_id UUID NOT NULL DEFAULT uuid_generate_v7 (),
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(1024) NOT NULL DEFAULT '',
+    priority INT NOT NULL DEFAULT 0,
+    completion_date TIMESTAMPTZ DEFAULT NULL,
+    parent_task_id UUID DEFAULT NULL REFERENCES planner.tasks (task_id),
+);
+
+DROP TABLE IF EXISTS planner.categories;
+
+CREATE TABLE planner.categories (
+    category_id UUID NOT NULL DEFAULT uuid_generate_v7 (),
+    name VARCHAR(64) NOT NULL,
+    PRIMARY KEY (category_id),
+)
+
+DROP TABLE IF EXISTS planner.task_categories;
+
+CREATE TABLE planner.task_categories (
+    task_id NOT NULL REFERENCES planner.tasks (task_id) ON DELETE CASCADE,
+    category_id NOT NULL REFERENCES planner.categories (category_id) ON DELETE CASCADE,
+    category_z_index INT NOT NULL DEFAULT 0,
+    PRIMARY KEY (task_id, category_id),
+);
