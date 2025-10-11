61243f10d6bf353d862170a6da6ff38e2582db44
Not_AI <44149574+N0tAI@users.noreply.github.com>
2025-10-11 13:19:45 -0400

    Added a TUnit test project

---
+++ src/server/Thayen.WebPlanner.Tests/WebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TUnit.Core.Interfaces;
+
+namespace Thayen.WebPlanner.Tests;
+
+public class WebApplicationFactory : WebApplicationFactory<Program>, IAsyncInitializer
+{
+    public Task InitializeAsync()
+    {
+        _ = Server;
+
+        return Task.CompletedTask;
+    }
+}
